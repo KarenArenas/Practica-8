d7f5be77400800c15781841f06998fbd918c94f2
KarenArenas <arenaskaren2005@gmail.com>
2024-10-04 13:55:14 -0600

    Create tablasdemultiplicar.c

---
+++ tablasdemultiplicar.c
@@ -0,0 +1,28 @@
+/*Arenas Mimbrera Karen Mariel
+Laboratorio Practica 8
+Tablas de Multiplicar*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void mult_for(int n)
+{
+  int resp;
+  
+  for(int i = 1; i <= 10; i++)
+  {
+    resp = n * i;
+    printf("%d * %d = %d\n", n, i, resp);
+  }
+}
+
+int main()
+{
+  int n;
+  
+  printf("Dame el numero de la tabla a multiplicar: ");
+  scanf("%d", &n);
+
+  mult_for(n);
+  return 0;
+}
